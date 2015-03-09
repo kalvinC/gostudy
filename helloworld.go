b526305f8968186b7768532f1dd7d0dcfd9f8edc
kalvinC <lklzyy@163.com>
2015-03-09 21:53:56 +0800

    first go program:hello world

---
+++ helloworld.go
@@ -0,0 +1,15 @@
+//please pay attention to this package.
+//Each program must have a main package.
+package main
+
+import(
+"fmt"
+)
+
+//this is first program.
+//any language study is started by this program.
+//if you want a program running, you must have a main entry.
+func main(){
+   fmt.Println("hello world")
+}
+
